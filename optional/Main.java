3617a763f3aeb6c46fa781764a78480e2c3c8c8c
Georgiana Druță <75542257+georgianadruta@users.noreply.github.com>
2021-04-02 21:28:09 +0300

    Delete optional directory

--- optional/Main.java
+++
@@ -1,9 +0,0 @@
-package optional;
-
-import optional.application.panels.MainFrame;
-
-public class Main {
-    public static void main(String[] args) {
-        new MainFrame().setVisible(true);
-    }
-}
